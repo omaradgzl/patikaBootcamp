41adb33e68bcf42514fa8355013c8a0c2232e62b
Raumdeuter <omaradgzl0@gmail.com>
2022-07-02 01:52:56 +0300

    basic solar system
    basic solar system with sun and 6 planets.

---
+++ solarSystem/Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    [Range(0,1)]
+    public float orbitalRotationSpeed;
+    [Range(0,1)]
+    public float selfRotationSpeed;
+    
+    private float dist;
+    private GameObject[] celestials;
+    private List<Vector3> iterations = new List<Vector3>();
+    private Vector3 sunPos;
+   
+
+    private void Awake()
+    {
+        celestials = GameObject.FindGameObjectsWithTag("Celestials");
+        sunPos = this.transform.position;
+    }
+
+    void Start()
+
+    {    
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.up, selfRotationSpeed);
+        for (int  i = 0; i < celestials.Length; i++)
+        {
+            dist = Vector3.Distance(sunPos, celestials[i].transform.position);
+            celestials[i].transform.RotateAround(this.transform.position, Vector3.up, (orbitalRotationSpeed*1000/dist) * Time.deltaTime);
+            celestials[i].transform.Rotate(Vector3.up, selfRotationSpeed);
+           
+        }
+        
+    }
+}
